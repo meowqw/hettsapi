88d595d45875f7bae61678de9b1570f924646bff
olega211 <106370448+olega211@users.noreply.github.com>
2023-05-14 15:21:52 +0700

    авторизация и регистрация

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['namespace'=> 'Product'], function () {
     Route::get('/products', 'IndexController');
 });
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        // Below mention routes are public, user can access those without any restriction.
+        // Create New User
+        Route::post('register', 'AuthController@register');
+        // Login User
+        Route::post('login', 'AuthController@login');
+
+        // Refresh the JWT Token
+        Route::get('refresh', 'AuthController@refresh');
+
+        // Below mention routes are available only for the authenticated users.
+        Route::middleware('auth:api')->group(function () {
+            // Get user info
+            Route::get('user', 'AuthController@user');
+            // Logout user from application
+            Route::post('logout', 'AuthController@logout');
+        });
+    });
+});
